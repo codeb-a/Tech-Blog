70743d28879cd95df8f26a53b08e6c4c588f8846
Scott Brunswig <sbrunswig@gmail.com>
2021-07-01 18:33:47 -0500

    hw 14 phase 1

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+// get all blog posts
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// one post
+
+// signup
+
+// login
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+// dashboard all posts
+
+// new post
+
+// edit post
+
+module.exports = router;
